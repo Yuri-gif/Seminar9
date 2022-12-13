2d619cfa78b7faa38e5064331601071464feb7f2
Yuri <urakokurin197@gmail.com>
2022-12-13 19:14:30 +0400

    first commit

---
+++ Задача 64/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите натуральное число больше 1:");
+int N = int.Parse(Console.ReadLine());
+
+///Метод вывода натуральных чисел от N до 1:
+void NumberCounter (int N )
+{
+    if (N < 0) Console.Write($"{N} не натуральное число");
+    if (N == 0) return;
+    Console.Write("{0,4}", N);
+    NumberCounter (N - 1);
+}
+
+NumberCounter(N);
